5806054cbea5e69fae66a0b6d622752ace690afd
Bogdan Popa <bogdan@defn.io>
2020-04-05 18:05:42 +0300

    base62: generate lookup tables at compile time

--- private/base62.rkt
+++ private/base62.rkt
@@ -1,33 +1,56 @@
 #lang racket/base
 
+(require (for-syntax racket/base
+                     syntax/parse))
+
 (provide
  number->base62-string
  base62-string->number)
 
-(define ALPHABET
-  (apply vector (string->list "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")))
+(begin-for-syntax
+  (define ALPHABET
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"))
 
-(define ALPHABET_REVERSE
-  (for/hasheqv ([(c i) (in-indexed ALPHABET)])
-    (values c i)))
+(define-syntax (make-lookup-table stx)
+  (syntax-parse stx
+    [(_ n:expr)
+     #:with ((v c) ...) (for/list ([(c i) (in-indexed ALPHABET)])
+                          (list (datum->syntax stx i)
+                                (datum->syntax stx c)))
+     #'(case n
+         [(v) c] ...
+         [else (raise-argument-error 'number->base62-string "a number in the range [0, 62)" n)])]))
+
+(define-syntax (make-reverse-table stx)
+  (syntax-parse stx
+    [(_ c:expr)
+     #:with ((v n) ...) (for/list ([(c i) (in-indexed ALPHABET)])
+                          (list (datum->syntax stx c)
+                                (datum->syntax stx i)))
+     #'(case c
+         [(v) n] ...
+         [else (raise-argument-error 'base62-string->number "a base62 character" c)])]))
+
+(define (number->char n)
+  (make-lookup-table n))
+
+(define (char->number c)
+  (make-reverse-table c))
 
 (define (number->base62-string n)
   (string->immutable-string
    (apply string (let loop ([n  n]
                             [cs null])
                    (if (< n 62)
-                       (cons (vector-ref ALPHABET n) cs)
+                       (cons (number->char n) cs)
                        (loop (quotient n 62)
-                             (cons (vector-ref ALPHABET (remainder n 62)) cs)))))))
+                             (cons (number->char (remainder n 62)) cs)))))))
 
 (define (base62-string->number s)
   (for/fold ([n 0])
             ([c (in-string s)])
-    (define d
-      (hash-ref ALPHABET_REVERSE c (lambda ()
-                                     (raise-argument-error 'base62-string->number "a base62 character" c))))
-
-    (+ (* n 62) d)))
+    (+ (* n 62)
+       (char->number c))))
 
 (module+ test
   (require rackcheck
