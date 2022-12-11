2349931cd105fc40212ed63bc34d7e451f4ad7c1
Bogdan Popa <bogdan@defn.io>
2022-12-11 18:17:13 +0200

    build: fix version

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.0.0")
+(define version "1.0")
 (define collection "buid")
 (define deps '("base"))
 (define build-deps '("racket-doc"
